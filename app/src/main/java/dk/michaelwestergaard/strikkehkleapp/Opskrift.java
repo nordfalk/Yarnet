52b9960f2f1f482613e896929aab68c0cfc13949
Alparslan B. Ari <burhanari@192.168.0.11>
2018-10-29 20:41:01 +0100

    Merge branch 'master' of /Users/burhanari/AndroidStudioProjects/Yarnet with conflicts.

---
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/Opskrift.java
@@ -0,0 +1,13 @@
+package dk.michaelwestergaard.strikkehkleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Opskrift extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opskrift);
+    }
+}
