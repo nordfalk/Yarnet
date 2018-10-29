067c75df678d1d527c3279a841abea123fa831d4
Michael Westergaard <michaelwestergaard@hotmail.dk>
2018-10-29 21:09:26 +0100

    Login som start og derefter gå til mainactivity

--- app/src/main/java/dk/michaelwestergaard/strikkehkleapp/logIn.java
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/logIn.java
@@ -2,12 +2,34 @@ package dk.michaelwestergaard.strikkehkleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
-public class logIn extends AppCompatActivity {
+import dk.michaelwestergaard.strikkehkleapp.activities.MainActivity;
+
+public class logIn extends AppCompatActivity implements View.OnClickListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Button loginBtn = findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.login_btn:
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+                break;
+            default:
+                break;
+        }
     }
 }
