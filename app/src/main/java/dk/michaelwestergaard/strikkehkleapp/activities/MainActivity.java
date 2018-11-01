e07501df875847b3a14660fa41b125d0f6ce0b45
lucassn2 <lugispil@spangholmnielsen.dk>
2018-11-01 14:49:11 +0100

    Drawer and bar layout Java main code for drawer Fragments added for drawer

--- app/src/main/java/dk/michaelwestergaard/strikkehkleapp/activities/MainActivity.java
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/activities/MainActivity.java
@@ -3,19 +3,30 @@ package dk.michaelwestergaard.strikkehkleapp.activities;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import dk.michaelwestergaard.strikkehkleapp.FavoritterFragment;
+import dk.michaelwestergaard.strikkehkleapp.IndstillingerFragment;
+import dk.michaelwestergaard.strikkehkleapp.OpretOpskriftFragment;
 import dk.michaelwestergaard.strikkehkleapp.R;
 import dk.michaelwestergaard.strikkehkleapp.ViewPagerAdapter;
+import dk.michaelwestergaard.strikkehkleapp.VisKontoFragment;
 import dk.michaelwestergaard.strikkehkleapp.fragments.DiscoverFragment;
 import dk.michaelwestergaard.strikkehkleapp.fragments.DiscoverStartFragment;
 import dk.michaelwestergaard.strikkehkleapp.fragments.ListFragment;
 
-public class MainActivity extends AppCompatActivity implements ListFragment.OnFragmentInteractionListener, DiscoverStartFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements ListFragment.OnFragmentInteractionListener, DiscoverStartFragment.OnFragmentInteractionListener,
+        NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout drawer;
     private ViewPager viewPager;
     BottomNavigationView bottomNavigationView;
 
@@ -25,6 +36,18 @@ public class MainActivity extends AppCompatActivity implements ListFragment.OnFr
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         viewPager = findViewById(R.id.mainViewPager);
         bottomNavigationView = findViewById(R.id.bottom_menu);
@@ -76,6 +99,14 @@ public class MainActivity extends AppCompatActivity implements ListFragment.OnFr
         setupViewPager(viewPager);
     }
 
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+        super.onBackPressed();
+    }
+
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new DiscoverFragment(), "Discover");
@@ -87,3 +118,34 @@ public class MainActivity extends AppCompatActivity implements ListFragment.OnFr
 
     }
 }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.nav_visKonto:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new VisKontoFragment()).commit();
+                break;
+            case R.id.nav_indstillinger:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new IndstillingerFragment()).commit();
+                break;
+            case R.id.nav_favoritter:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FavoritterFragment()).commit();
+                break;
+            case R.id.nav_opretOpskrift:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new OpretOpskriftFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
