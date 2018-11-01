76cfea59f18012bf027809409b9db39e12c69601
Alparslan B. Ari <burhanari@192.168.0.11>
2018-11-01 21:07:04 +0100

    Merge branch 'master' of /Users/burhanari/AndroidStudioProjects/Yarnet with conflicts.

--- app/src/main/java/dk/michaelwestergaard/strikkehkleapp/OpretOpskriftFragment.java
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/OpretOpskriftFragment.java
@@ -6,12 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class OpretOpskriftFragment extends Fragment {
+public class OpretOpskriftFragment extends Fragment implements View.OnClickListener {
+    Button opret;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_opretopskrift, container, false);
+
+        opret = (Button) ;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
---
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/fragments/MinSamling.java
@@ -0,0 +1,15 @@
+package dk.michaelwestergaard.strikkehkleapp.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dk.michaelwestergaard.strikkehkleapp.R;
+
+public class MinSamling extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_min_samling);
+    }
+}
