3817f53a9c79a60a2a855ccc928368b628f7170d
Michael Westergaard <michaelwestergaard@hotmail.dk>
2018-11-01 21:53:26 +0100

    Fix af alps commit

--- app/src/main/java/dk/michaelwestergaard/strikkehkleapp/Opskrift.java
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/Opskrift.java
@@ -1,23 +1,21 @@
 package dk.michaelwestergaard.strikkehkleapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class Opskrift extends AppCompatActivity implements View.OnClickListener {
 
-
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opskrift);
+        title = findViewById(R.id.recipe_title);
 
-
-
-
-
-
+        title.setText(getIntent().getStringExtra("Title"));
     }
 
     @Override
