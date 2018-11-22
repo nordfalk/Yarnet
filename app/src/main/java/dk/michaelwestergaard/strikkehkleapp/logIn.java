a8c8474394d1b75df260ee1b8fab7e256a7fddc0
Michael Westergaard <michaelwestergaard@hotmail.dk>
2018-11-23 00:18:48 +0100

    Tilføjet opret og log ind med firebase auth

--- app/src/main/java/dk/michaelwestergaard/strikkehkleapp/logIn.java
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/logIn.java
@@ -1,32 +1,71 @@
 package dk.michaelwestergaard.strikkehkleapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import dk.michaelwestergaard.strikkehkleapp.activities.MainActivity;
 
 public class logIn extends AppCompatActivity implements View.OnClickListener {
 
+    private FirebaseAuth auth;
 
+    private EditText inputEmail, inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+
+        inputEmail = findViewById(R.id.input_email);
+
         Button loginBtn = findViewById(R.id.login_btn);
         loginBtn.setOnClickListener(this);
+
+        Button signupBtn = findViewById(R.id.signup_btn);
+        signupBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.login_btn:
-                Intent i = new Intent(this, MainActivity.class);
-                startActivity(i);
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(logIn.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            // tjek fejlen
+                        } else {
+                            Intent i = new Intent(logIn.this, MainActivity.class);
+                            startActivity(i);
+                            finish();
+                        }
+                    }
+                });
+                break;
+
+            case R.id.signup_btn:
+                Intent signupIntent = new Intent(this, signUp.class);
+                startActivity(signupIntent);
                 break;
             default:
                 break;
--- app/src/main/java/dk/michaelwestergaard/strikkehkleapp/signUp.java
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/signUp.java
@@ -1,13 +1,88 @@
 package dk.michaelwestergaard.strikkehkleapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class signUp extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class signUp extends AppCompatActivity implements View.OnClickListener, OnCompleteListener<AuthResult> {
+
+    private FirebaseAuth auth;
+
+    private EditText inputFirstName, inputLastName, inputEmail, inputPassword, inputPasswordAgain;
+    private Button btnSignup;
+
+    AlertDialog.Builder builder;
+    AlertDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        auth = FirebaseAuth.getInstance();
+
+        inputFirstName      = findViewById(R.id.input_first_name);
+        inputLastName       = findViewById(R.id.input_first_last);
+        inputEmail          = findViewById(R.id.input_email);
+        inputPassword       = findViewById(R.id.input_password);
+        inputPasswordAgain  = findViewById(R.id.input_password_again);
+
+        btnSignup = findViewById(R.id.btn_signup);
+        btnSignup.setOnClickListener(this);
+
+        //Loading dialog
+        builder = new AlertDialog.Builder(signUp.this);
+        builder.setCancelable(false); // if you want user to wait for some process to finish,
+        builder.setView(R.layout.loading_dialog);
+        progressDialog = builder.create();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnSignup) {
+            progressDialog.show();
+            validateInputs();
+        }
+    }
+
+    private boolean validateInputs(){
+
+        if(!isEmpty(inputFirstName) && !isEmpty(inputLastName) && !isEmpty(inputEmail) && !isEmpty(inputPassword) && !isEmpty(inputPasswordAgain)){
+            if(inputPassword.getText().toString().equals(inputPasswordAgain.getText().toString())){
+                //All good?
+                // TODO: Gem brugeroplysningerne sammen med det userid der bliver lavet
+                auth.createUserWithEmailAndPassword(inputEmail.getText().toString(), inputPassword.getText().toString()).addOnCompleteListener(this);
+            }
+        }
+        progressDialog.dismiss();
+        return false;
+    }
+
+    private boolean isEmpty(EditText editText){
+        return editText.getText().toString().equals("");
+    }
+
+    @Override
+    public void onComplete(@NonNull Task<AuthResult> task) {
+
+        progressDialog.dismiss();
+
+        Toast.makeText(signUp.this, "Du er hermed oprettet som bruger!", Toast.LENGTH_SHORT).show();
+
+        if (!task.isSuccessful()) {
+            Toast.makeText(signUp.this, "Kunne ikke oprette, prøv igen.", Toast.LENGTH_SHORT).show();
+        } else {
+            finish();
+        }
     }
 }
