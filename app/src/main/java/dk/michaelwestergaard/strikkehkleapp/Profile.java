2c6c028546bac95f4a7b140ccbf5d2ffdabbbf73
Michael Westergaard <michaelwestergaard@hotmail.dk>
2018-11-26 11:20:13 +0100

    Revert "Log ind og opret færdig"

--- app/src/main/java/dk/michaelwestergaard/strikkehkleapp/Profile.java
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/Profile.java
@@ -1,62 +1,13 @@
 package dk.michaelwestergaard.strikkehkleapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.os.Bundle;
 
-import com.facebook.login.LoginManager;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-public class Profile extends AppCompatActivity implements View.OnClickListener {
-
-    Button logout;
-    private FirebaseAuth.AuthStateListener authListener;
-    private FirebaseAuth auth;
-
-    ImageView avatar;
-    TextView name;
+public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        auth = FirebaseAuth.getInstance();
-
-        avatar = findViewById(R.id.avatar);
-        name = findViewById(R.id.profileName);
-
-/*
-        Picasso.get().load(user.getAvatar()).transform(new RoundedCornersTransformation(50, 50)).into(avatar);
-        name.setText(user.getFirst_name() + " " + user.getFirst_name());
-*/
-        logout = findViewById(R.id.logout);
-        logout.setOnClickListener(this);
-
-        authListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user == null) {
-                    startActivity(new Intent(Profile.this, logIn.class));
-                    finish();
-                }
-            }
-        };
-
-        auth.addAuthStateListener(authListener);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if(view == logout){
-            auth.signOut();
-            LoginManager.getInstance().logOut();
-        }
     }
 }
