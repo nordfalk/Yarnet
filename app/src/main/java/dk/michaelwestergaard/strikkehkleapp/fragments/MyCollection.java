6a3c198a5d3edb68b879e0f49e7d4a956bfe53c4
Mikkel Berg Eriksen <eriksen127@msn.com>
2019-01-17 15:36:07 +0100

    Opskrifter bliver vist (alle sammen + usorteret) i MinSamling

--- app/src/main/java/dk/michaelwestergaard/strikkehkleapp/fragments/MyCollection.java
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/fragments/MyCollection.java
@@ -3,6 +3,7 @@ package dk.michaelwestergaard.strikkehkleapp.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,8 +11,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dk.michaelwestergaard.strikkehkleapp.DAO.RecipeDAO;
+import dk.michaelwestergaard.strikkehkleapp.DTO.RecipeDTO;
 import dk.michaelwestergaard.strikkehkleapp.ListAdapter;
 import dk.michaelwestergaard.strikkehkleapp.R;
+import dk.michaelwestergaard.strikkehkleapp.adapters.RecipeAdapter;
 
 
 /**
@@ -27,6 +38,8 @@ public class MyCollection extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private RecipeDAO recipeDAO = new RecipeDAO();
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -67,24 +80,43 @@ public class MyCollection extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_my_collection, container, false);
+        final View view = inflater.inflate(R.layout.fragment_my_collection, container, false);
 
-        ListAdapter listAdapter = new ListAdapter();
+        final List<RecipeDTO> recipes = new ArrayList<RecipeDTO>();
 
-        RecyclerView recyclerViewSaved = view.findViewById(R.id.SavedPatternsView);
-        RecyclerView.LayoutManager layoutManagerNew = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
-        recyclerViewSaved.setAdapter(listAdapter);
-        recyclerViewSaved.setLayoutManager(layoutManagerNew);
+        recipeDAO.getReference().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                recipes.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    recipes.add(snapshot.getValue(RecipeDTO.class));
+                }
 
-        RecyclerView recyclerViewBought = view.findViewById(R.id.BoughtPatternsView);
-        RecyclerView.LayoutManager layoutManagerPaid = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
-        recyclerViewBought.setAdapter(listAdapter);
-        recyclerViewBought.setLayoutManager(layoutManagerPaid);
+                RecipeAdapter adapterSaved = new RecipeAdapter(recipes);
+                RecipeAdapter adapterBought = new RecipeAdapter(recipes);
+                RecipeAdapter adapterMy = new RecipeAdapter(recipes);
 
-        RecyclerView recyclerViewMy = view.findViewById(R.id.MyPatternsView);
-        RecyclerView.LayoutManager layoutManagerFree = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
-        recyclerViewMy.setAdapter(listAdapter);
-        recyclerViewMy.setLayoutManager(layoutManagerFree);
+                RecyclerView recyclerViewSaved = view.findViewById(R.id.SavedPatternsView);
+                RecyclerView.LayoutManager layoutManagerNew = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+                recyclerViewSaved.setAdapter(adapterSaved);
+                recyclerViewSaved.setLayoutManager(layoutManagerNew);
+
+                RecyclerView recyclerViewBought = view.findViewById(R.id.BoughtPatternsView);
+                RecyclerView.LayoutManager layoutManagerPaid = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+                recyclerViewBought.setAdapter(adapterBought);
+                recyclerViewBought.setLayoutManager(layoutManagerPaid);
+
+                RecyclerView recyclerViewMy = view.findViewById(R.id.MyPatternsView);
+                RecyclerView.LayoutManager layoutManagerFree = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+                recyclerViewMy.setAdapter(adapterMy);
+                recyclerViewMy.setLayoutManager(layoutManagerFree);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
 
         return view;
     }
