1c66a7f842869ed563a5a3dc17be7861e004a8ad
Alparslan B. Ari <alparslanba58@hotmail.com>
2019-01-18 23:20:37 +0100

    Spinneren for valg af opskrifttyper implementeret, men lige en lille fejl på sværhedsgraden skal fikses

--- app/src/main/java/dk/michaelwestergaard/strikkehkleapp/activities/Settings.java
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/activities/Settings.java
@@ -3,19 +3,42 @@ package dk.michaelwestergaard.strikkehkleapp.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
+import dk.michaelwestergaard.strikkehkleapp.DAO.RecipeDAO;
+import dk.michaelwestergaard.strikkehkleapp.DAO.UserDAO;
+import dk.michaelwestergaard.strikkehkleapp.DTO.RecipeDTO;
+import dk.michaelwestergaard.strikkehkleapp.DTO.UserDTO;
 import dk.michaelwestergaard.strikkehkleapp.R;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     ImageView backBtn;
     ImageView drawerBtn;
+    Spinner difficultySpinner, recipeTypeSpinner;
+    RecipeDTO recipeDTO = new RecipeDTO();
+    RecipeDAO recipeDAO = new RecipeDAO();
+    UserDAO userDAO = new UserDAO();
+    UserDTO userDTO = new UserDTO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        difficultySpinner = findViewById(R.id.difficultySpinner);
+        recipeTypeSpinner = findViewById(R.id.recipeSpinner);
+
+        ArrayAdapter<CharSequence> recAdapter = ArrayAdapter.createFromResource(this,
+                R.array.RecipeTypes, android.R.layout.simple_spinner_item);
+
+        ArrayAdapter<CharSequence> difAdapter = ArrayAdapter.createFromResource(this,
+                R.array.NewRecipeDifficulty, android.R.layout.simple_spinner_item);
+
+        recipeTypeSpinner.setAdapter(recAdapter);
+        difficultySpinner.setAdapter(difAdapter);
 
         drawerBtn = findViewById(R.id.drawerBtn);
         backBtn = findViewById(R.id.backButton);
@@ -28,5 +51,36 @@ public class Settings extends AppCompatActivity {
 
         backBtn.setVisibility(View.VISIBLE);
         drawerBtn.setVisibility(View.GONE);
+
+        recipeTypeSpinner.setOnItemSelectedListener(this);
+        difficultySpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        parent.getItemAtPosition(position);
+
+        if (difficultySpinner.getSelectedItemPosition()==0){
+            recipeDTO.setRecipeDifficulty(RecipeDTO.RecipeDifficulty.EASY);
+        }else if(difficultySpinner.getSelectedItemPosition()==1){
+            recipeDTO.setRecipeDifficulty(RecipeDTO.RecipeDifficulty.MEDIUM);
+        }else if(difficultySpinner.getSelectedItemPosition()==2) {
+            recipeDTO.setRecipeDifficulty(RecipeDTO.RecipeDifficulty.HARD);
+        }
+        //recipeDAO.update(recipeDTO);
+
+        if (recipeTypeSpinner.getSelectedItemPosition()==0) {
+            userDTO.setType("ALL");
+        } else if (recipeTypeSpinner.getSelectedItemPosition()==1) {
+            userDTO.setType("CROCHETING");
+        } else if (recipeTypeSpinner.getSelectedItemPosition()==2) {
+            userDTO.setType("KNITTING");
+        }
+        userDAO.update(userDTO);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
