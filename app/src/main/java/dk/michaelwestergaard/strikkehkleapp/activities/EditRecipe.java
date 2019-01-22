149120c2bf4187b3aaffbed36dfad3daefdda54d
Mikkel Berg Eriksen <eriksen127@msn.com>
2019-01-22 12:00:38 +0100

    EditRecipe update: overflødige imports fjernet

--- app/src/main/java/dk/michaelwestergaard/strikkehkleapp/activities/EditRecipe.java
+++ app/src/main/java/dk/michaelwestergaard/strikkehkleapp/activities/EditRecipe.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Handler;
-import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,7 +12,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
@@ -21,7 +19,6 @@ import com.stepstone.stepper.BlockingStep;
 import com.stepstone.stepper.StepperLayout;
 import com.stepstone.stepper.VerificationError;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
